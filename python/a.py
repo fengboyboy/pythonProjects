12debad5c3e49c206c5f3d00fb1751d36c80e322
CGM <fengchuanhuan@163.com>
2018-08-02 20:46:58 +0800

    测试1

--- python/a.py
+++ python/a.py
@@ -3,11 +3,12 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
+html = urlopen(
+    "https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
 print(html)
 
-soup = BeautifulSoup(html, features = 'lxml')
-# print(soup.h1)
+soup = BeautifulSoup(html, features='lxml')
+print(soup.h1)
 
 all_href = soup.find_all('a')
 all_href = [l['href'] for l in all_href]
